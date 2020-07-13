5967b3ff260e1824829ba433014b18f0b3ee9bb3
Robert Andrew Parkinson <R.Stonehouse8382@student.leedsbeckett.ac.uk>
2020-07-13 16:04:45 +0100

    Add project files.

---
+++ EKM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EKM.MvcApplication" Language="C#" %>
---
+++ EKM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EKM.Startup))]
+namespace EKM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
